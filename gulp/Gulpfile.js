909efbc874cab6686c4a5e5da48ae5872b461727
feelfree7 <denis_teles@yahoo.com>
2024-06-19 22:07:20 +0300

    pirmas testas

--- gulp/Gulpfile.js
+++ gulp/Gulpfile.js
@@ -1,15 +1,19 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
+const cleanCSS = require('gulp-clean-css')
+const concatCSS = require('gulp-concat-css')
+const rename = require('gulp-rename')
 
 
 function styles(cb) {
-    gulp.src('src/sass/**/.scss')
-    .pipe(sass().on('error', sass.logError))
+    gulp.src('src/sass/**/*.scss')
+    .pipe(sass().on('error',sass.logError))
+    .pipe(concatCSS('style.css', {'rebaseUrls':false}))
+    .pipe(cleanCSS())
+    .pipe(rename({basename:'style', suffix:'.min'}))
     .pipe(gulp.dest('dest/'))
-    
+    cb() 
 }
-
-
 
 
 function copyHtmlFile(cb) {
@@ -18,12 +22,21 @@ function copyHtmlFile(cb) {
     cb()
 }
 
-function watch(cb) {
+
+function watch (cb) {
     gulp.watch('src/**/*.html', gulp.series(copyHtmlFile))
-gulp.watch('src/sass/**/*.scss', gulp.series(styles))
+    gulp.watch('src/sass/**/*.scss', gulp.series(styles))
+    cb()
 }
+
+
 
 exports.styles = styles
 exports.watch = watch
 exports.copyHtmlFile = copyHtmlFile
-exports.default = gulp.series(gulp.parallel(copyHtmlFile, styles), watch)
+exports.default = gulp.series(
+gulp.parallel(copyHtmlFile, styles),
+watch 
+)
+
+
